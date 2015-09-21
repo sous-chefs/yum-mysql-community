865822c6acd03393e1245950ff85d62a259c5709
Tim Smith <tsmith84@gmail.com>
2015-09-21 16:39:22 -0700

    Release v0.1.18

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
 description 'Installs/Configures yum-mysql-community'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.17'
+version '0.1.18'
 
 depends 'yum', '>= 3.0'
 
