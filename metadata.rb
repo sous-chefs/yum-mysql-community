098870e9e78cd8cf458bc12580f94d9250a73d5a
Tim Smith <tsmith84@gmail.com>
2016-03-29 11:32:14 -0700

    Release 0.2.0

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
 description 'Installs/Configures yum-mysql-community'
-version '0.1.21'
+version '0.2.0'
 
 depends 'yum', '>= 3.2'
 
