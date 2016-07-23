9cefa7143b5e24db7e6d7c6e1577b5767ef30856
Tim Smith <tsmith@chef.io>
2016-07-22 20:30:14 -0700

    Add chef_version metadata
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,7 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
 description 'Installs/Configures yum-mysql-community'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.2.0'
 
 depends 'yum', '>= 3.2'
@@ -13,3 +14,5 @@ end
 
 source_url 'https://github.com/chef-cookbooks/yum-mysql-community' if respond_to?(:source_url)
 issues_url 'https://github.com/chef-cookbooks/yum-mysql-community/issues' if respond_to?(:issues_url)
+
+chef_version '>= 11.0' if respond_to?(:chef_version)
