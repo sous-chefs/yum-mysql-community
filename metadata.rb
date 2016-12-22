f3bef4f822a643b6c76c92316336963b69940f17
Tim Smith <tsmith@chef.io>
2016-12-22 10:34:57 -0800

    Depend on the latest compat_resource cookbook

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description 'Installs/Configures yum-mysql-community'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '2.0.2'
 
-depends 'compat_resource', '>= 12.14.7'
+depends 'compat_resource', '>= 12.16.3'
 
 %w(amazon centos fedora oracle redhat scientific).each do |os|
   supports os
