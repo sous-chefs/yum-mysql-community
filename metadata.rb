068a7f0c39ebd8ac5d6c3c2e996022ef0a67c305
Tim Smith <tsmith@chef.io>
2017-03-21 15:57:49 -0700

    Update apache2 license string
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name 'yum-mysql-community'
 maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
-license 'Apache 2.0'
+license 'Apache-2.0'
 description 'Installs/Configures yum-mysql-community'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '2.0.3'
