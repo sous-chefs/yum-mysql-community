5df3c49c12f2d3f9568935c06cd5d546b224b934
Lance Albertson <lance@osuosl.org>
2020-10-25 15:31:09 -0700

    Cleanup attributes and fix Fedora
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- attributes/mysql56-community.rb
+++ attributes/mysql56-community.rb
@@ -7,28 +7,17 @@ default['yum']['mysql56-community']['enabled'] = true
 
 case node['platform_family']
 when 'rhel'
-  if platform?('redhat', 'oracle')
-    case node['platform_version'].to_i
-    when 5
-      # Real Redhat identifies $releasever as 5Server and 6Server
-      default['yum']['mysql56-community']['baseurl'] = 'http://repo.mysql.com/yum/mysql-5.6-community/el/5/$basearch/'
-    when 6
-      default['yum']['mysql56-community']['baseurl'] = 'http://repo.mysql.com/yum/mysql-5.6-community/el/6/$basearch/'
-    when 7
-      default['yum']['mysql56-community']['baseurl'] = 'http://repo.mysql.com/yum/mysql-5.6-community/el/7/$basearch/'
-    end
-  else # other rhel
-    default['yum']['mysql56-community']['baseurl'] = 'http://repo.mysql.com/yum/mysql-5.6-community/el/$releasever/$basearch/'
-  end
+  default['yum']['mysql56-community']['baseurl'] = 'http://repo.mysql.com/yum/mysql-5.6-community/el/$releasever/$basearch/'
 when 'fedora'
-  default['yum']['mysql56-community']['baseurl'] = 'http://repo.mysql.com/yum/mysql-5.6-community/fc/$releasever/$basearch/'
+  default['yum']['mysql56-community']['baseurl'] = 'http://repo.mysql.com/yum/mysql-5.6-community/el/7/$basearch/'
 when 'amazon'
-  default['yum']['mysql56-community']['baseurl'] = case node['platform_version'].to_i
-                                                   when /201./
-                                                     'http://repo.mysql.com/yum/mysql-5.6-community/el/6/$basearch/'
-                                                   when 2
-                                                     'http://repo.mysql.com/yum/mysql-5.6-community/el/7/$basearch/'
-                                                   else
-                                                     'http://repo.mysql.com/yum/mysql-5.6-community/el/6/$basearch/'
-                                                   end
+  default['yum']['mysql56-community']['baseurl'] =
+    case node['platform_version'].to_i
+    when /201./
+      'http://repo.mysql.com/yum/mysql-5.6-community/el/6/$basearch/'
+    when 2
+      'http://repo.mysql.com/yum/mysql-5.6-community/el/7/$basearch/'
+    else
+      'http://repo.mysql.com/yum/mysql-5.6-community/el/6/$basearch/'
+    end
 end
