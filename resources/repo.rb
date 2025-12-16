39e90b4fb9c4833877ca0c8364fd23c8cab3784b
gwhelan <gwhelan@cargurus.com>
2025-12-16 13:39:12 -0500

    errant space

--- resources/repo.rb
+++ resources/repo.rb
@@ -82,7 +82,7 @@ action :create do
 
   yum_repository 'mysql-tools-community' do
     description 'MySQL Tools Community'
-    if  new_resource.version.to_f < 8.4
+    if new_resource.version.to_f < 8.4
       baseurl "https://repo.mysql.com/yum/mysql-tools-community/#{os}/#{os_ver}/$basearch/"
     else
       baseurl "https://repo.mysql.com/yum/mysql-tools-#{new_resource.version}-community/#{os}/#{os_ver}/$basearch/"
